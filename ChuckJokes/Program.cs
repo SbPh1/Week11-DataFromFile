dc52bef80fde431c5d6acd98bf359d2231c12f1d
H <sikkelh@gmail.com>
2021-11-16 21:56:27 +0200

    Data From File

---
+++ ChuckJokes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckJokes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(RandomLineFromFile());
+            }
+
+        }
+        private static string RandomLineFromFile()
+        {
+            string filepath = @"C:\Users\monkey\samples\ChuckJokes.txt";
+            string[] dataFromFile = File.ReadAllLines(filepath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, dataFromFile.Length);
+            return dataFromFile[randomIndex];
+        }
+    }
+    
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Horoscope:");
+            string userHoroscope = Console.ReadLine();
+            string output = GetLineFromfileData(userHoroscope);
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry.The horoscope does not exist.");
+            }
+        }
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\monkey\samples";
+            string filename = "horoscope.txt";
+            string fullpath = $@"{directoryPath}\{filename}";
+            string[] dataFromFile = File.ReadAllLines(fullpath);
+            return dataFromFile;
+        }
+        private static string GetLineFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
